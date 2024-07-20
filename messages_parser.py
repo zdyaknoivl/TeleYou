1ec949bc7ca5a1690c3561bbfd87cec9163d7b48
zdyaknoivl <itsoke@mail.ru>
2024-07-21 02:45:30 +0300

    first commit

---
+++ messages_parser.py
@@ -0,0 +1,7 @@
+class MessagesParser:
+    def __init__(self, src_path, output_path):
+        self.src_path = src_path
+        self.output_path = output_path
+
+    def parse_messages(self):
+        pass
